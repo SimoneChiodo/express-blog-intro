0ccb9af011f1784096634546e6e3a926e769865a
SimoneChiodo <simochi.005@gmail.com>
2024-11-19 15:50:50 +0100

    add home route

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("ciao");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.type("html").send("Server del mio blog");
+});
+
+app.listen(port, () => {
+    console.log(`App Express listening on port ${port}`);
+});

35b6464053e3a4a88a1b85d5ba3c6d8fede9aca3
SimoneChiodo <simochi.005@gmail.com>
2024-11-19 16:41:21 +0100

    remove dynamic-post-creation

--- index.js
+++ index.js
@@ -22,29 +22,74 @@ app.listen(port, () => {
     console.log(`App Express listening on port ${port}`);
 });
 
-// Dichiarazione oggetto Post
-let post = {
-    titolo: "",
-    contenuto: "",
-    immagine: "",
-    tag: [],
-};
-
-// Array di post
-let posts = [];
-for (let i = 0; i < 5; i++) {
-    addObjectToArray(posts, post);
-}
-posts[0].immagine = "./images/ciambellone.jpeg";
-
+// Dichiarazione dei post
+let posts = [
+    // Post 1
+    {
+        titolo: "Ciambellone",
+        contenuto: "",
+        immagine: "./images/ciambellone.jpeg",
+        tag: ["ciambella", "zucchero a velo", "dolce"],
+    },
+    // Post 2
+    {
+        titolo: "Cracker di barbabietola",
+        contenuto: "",
+        immagine: "./images/cracker_barbabietola.jpeg",
+        tag: ["cracker", "barbabietola"],
+    },
+    // Post 3
+    {
+        titolo: "Pane fritto dolce",
+        contenuto: "",
+        immagine: "./images/pane_fritto_dolce.jpeg",
+        tag: ["pane", "fritto", "dolce"],
+    },
+    // Post 4
+    {
+        titolo: "Pasta di barbabietola",
+        contenuto: "",
+        immagine: "./images/pasta_barbabietola.jpeg",
+        tag: ["pasta", "barbabietola"],
+    },
+    // Post 5
+    {
+        titolo: "Torta paesana",
+        contenuto: "",
+        immagine: "./images/torta_paesana.jpeg",
+        tag: ["torta", "dolce"],
+    },
+];
 console.log(posts);
 
-/**
- * Funzione che aggiunge un oggetto a un array un oggetti.
- *
- * @param {Array} array array a cui aggiugere l'oggetto
- * @param {Object} object oggetto da aggiungere
- */
-function addObjectToArray(array, object) {
-    array.push({ ...object });
-}
+// REMOVED: CREAZIONE DINAMICA DI POST
+//
+//   ||
+//   ||
+//  \  /
+//   \/
+//
+// // Dichiarazione oggetto Post
+// let post = {
+//     titolo: "",
+//     contenuto: "",
+//     immagine: "",
+//     tag: [],
+// };
+
+// // Array di post
+// let posts = [];
+// for (let i = 0; i < 5; i++) {
+//     addObjectToArray(posts, post);
+// }
+// posts[0].immagine = "./images/ciambellone.jpeg";
+
+// /**
+//  * Funzione che aggiunge un oggetto a un array un oggetti.
+//  *
+//  * @param {Array} array array a cui aggiugere l'oggetto
+//  * @param {Object} object oggetto da aggiungere
+//  */
+// function addObjectToArray(array, object) {
+//     array.push({ ...object });
+// }

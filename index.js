7185eb6eee64cf025f0d6b8d85ac08150f9a6b6f
SimoneChiodo <simochi.005@gmail.com>
2024-11-19 16:25:52 +0100

    add Bacheca route

--- index.js
+++ index.js
@@ -2,16 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Pagina Home
-app.get("/", (req, res) => {
-    res.type("html").send("Server del mio blog");
-});
-
-app.listen(port, () => {
-    console.log(`App Express listening on port ${port}`);
-});
-
-// Oggetto Post
+// Dichiarazione oggetto Post
 let post = {
     titolo: "",
     contenuto: "",
@@ -19,11 +10,29 @@ let post = {
     tag: [],
 };
 
+// Pagina: Home
+app.get("/", (req, res) => {
+    res.type("html").send("Server del mio blog");
+});
+
+// Pagina: Bacheca
+app.get("/bacheca", (req, res) => {
+    res.type("json").send({
+        totalePost: posts.length,
+        listaPost: posts,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App Express listening on port ${port}`);
+});
+
 // Array di post
 let posts = [];
 for (let i = 0; i < 5; i++) {
     addObjectToArray(posts, post);
 }
+
 console.log(posts);
 
 /**
